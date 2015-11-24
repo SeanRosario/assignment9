c07c73b7e95af549170321ff3de9c2d210022578
Brian Karfunkel <bk1051@nyu.edu>
2015-11-24 12:36:59 -0500

    Update data explorer

--- DataExplorer.py
+++ DataExplorer.py
@@ -5,14 +5,23 @@ import matplotlib.pyplot as plt
 class DataExplorer(object):
     '''Manage exploratory data analysis'''
 
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, year=None):
+        self.year = year
         self.data = dataframe
 
     def plot(self):
         self.data.hist(by="Region", sharex=True, figsize=(10, 8), layout=(2, 3))
+        plt.title("Distribution of Countries' GDP per Capita, %s" % self.year)
+        plt.xlabel("Income (Log Scale)")
+        xticks = np.arange(2, 5.5, 0.25)
+        #xtick_labels = [100, 500, 1000, 5000, 7500, 10000, 25000, 50000, 75000, 100000]
+        xtick_labels = (10**xticks).astype('int')
+        plt.xticks(xticks, xtick_labels)
+        plt.yticks(np.arange(0, 55, 5))
         hist_fig = plt.gcf() # Get current figure
 
         self.data.boxplot(by="Region", showmeans=True)
+        plt.yticks([100, 500, 1000, 5000, 7500, 10000, 25000, 50000, 75000, 100000])
         box_fig = plt.gcf()
         # Clean labels
         locs, labels = plt.xticks()
--- IncomeDataController.py
+++ IncomeDataController.py
@@ -38,9 +38,20 @@ class IncomeDataController(object):
 
     def plot_income(self, year):
         '''Plot a historgram of the income distributions for a given year'''
-        plt.figure()
-        self.income[year].hist()
-        plt.title("Distribution of Countries' GDP per Capita, %d" % year)
+        fig, ax = plt.subplots()
+
+        xticks = np.arange(2, 5.5, 0.25)
+        #xtick_labels = [100, 500, 1000, 5000, 7500, 10000, 25000, 50000, 75000, 100000]
+        xtick_labels = (10**xticks).astype('int')
+
+        np.log10(self.income[year]).hist(ax=ax, bins=xticks)
+        plt.title("Distribution of Countries' GDP per Capita, %s" % year)
+        plt.xlabel("Income (Log Scale)")
+        #xtick_labels = [100, 500, 1000, 5000, 7500, 10000, 25000, 50000, 75000, 100000]
+
+        plt.xticks(xticks, xtick_labels, rotation=90)
+        #plt.xticks(np.log10(xtick_labels), xtick_labels)
+        plt.yticks(np.arange(0, 55, 5))
         plt.show()
 
     def merge_by_year(self, year):
@@ -56,7 +67,7 @@ class IncomeDataController(object):
         '''Loop through years and plot each one, saving results to files'''
         for year in years:
             merged = self.merge_by_year(year)
-            explorer = de.DataExplorer(merged)
+            explorer = de.DataExplorer(merged, year)
             explorer.save_plots(
                 os.path.join(self.output_dir, 'hist%d.png' % year),
                 os.path.join(self.output_dir, 'boxplot%d.png' % year)
