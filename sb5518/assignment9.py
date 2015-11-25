dc8eff2591392cc64bada9c7edbaf253d731f0b5
sb5518 <sb5518@nyu.edu>
2015-11-25 11:45:31 -0500

    Third commit

--- sb5518/assignment9.py
+++ sb5518/assignment9.py
@@ -20,9 +20,12 @@ Please make sure that the files 'countries.csv' and 'indicator gapminder gdp_per
 
 import loaderandmerger
 import plot_income_by_region
+from loaderandmerger import MyError as loader_Error
 
-list_of_valid_years = ('1800','1801','1802','1803','1804','1805','1806','1807','1808','1809','1810','1811','1812','1813','1814','1815','1816','1817','1818','1819','1820','1821','1822','1823','1824','1825','1826','1827','1828','1829','1830','1831','1832','1833','1834','1835','1836','1837','1838','1839','1840','1841','1842','1843','1844','1845','1846','1847','1848','1849','1850','1851','1852','1853','1854','1855','1856','1857','1858','1859','1860','1861','1862','1863','1864','1865','1866','1867','1868','1869','1870','1871','1872','1873','1874','1875','1876','1877','1878','1879','1880','1881','1882','1883','1884','1885','1886','1887','1888','1889','1890','1891','1892','1893','1894','1895','1896','1897','1898','1899','1900','1901','1902','1903','1904','1905','1906','1907','1908','1909','1910','1911','1912','1913','1914','1915','1916','1917','1918','1919','1920','1921','1922','1923','1924','1925','1926','1927','1928','1929','1930','1931','1932','1933','1934','1935','1936','1937','1938','1939','1940','1941','1942','1943','1944','1945','1946','1947','1948','1949','1950','1951','1952','1953','1954','1955','1956','1957','1958','1959','1960','1961','1962','1963','1964','1965','1966','1967','1968','1969','1970','1971','1972','1973','1974','1975','1976','1977','1978','1979','1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012')
-list_of_years_graph_explorer = (2007, 2008, 2009 , 2010 , 2011 , 2012 )
+
+list_of_valid_years = list() #generate a list of valid strings that represent the available data.
+for i in range(1800, 2013):
+    list_of_valid_years.append(str(i))
 
 is_input_correct = False
 
@@ -34,12 +37,14 @@ while is_input_correct == False:    #Loop until we get a valid input year
         if user_input in list_of_valid_years:
                 is_input_correct = True
                 year = int(user_input)
-                loaderandmerger.income_distribution_hist(year)
+                loaderandmerger.loader_merger_histogram.income_distribution_hist(year)
         else:
                 print "Please enter a valid year from 1800 to 2012"
 
     except (KeyboardInterrupt, EOFError): #avoid interrupting the program
         continue
+    except loader_Error as e:
+        print str(e)
 
 user_input_2 = "hey"
 
@@ -52,14 +57,19 @@ while user_input_2 <> "finish":  #Loop until user inputs 'finish'
 
         if user_input_2 in  list_of_valid_years:
             year = int(user_input_2)
-            loaderandmerger.income_distribution_hist(year)
+            loaderandmerger.loader_merger_histogram.income_distribution_hist(year)
 
         elif ((user_input_2 not in list_of_valid_years) and (user_input_2!="finish")):
             print "Please enter a valid year from 1800 to 2012"
 
     except (KeyboardInterrupt, EOFError): #avoid interrupting the program
         continue
+    except loader_Error as e:
+            print str(e)
 
 
-for year in list_of_years_graph_explorer:
-    plot_income_by_region.graphical_explorer(year)
+for year in range(2007, 2013):   #plot histograms and boxplots from years 2007-2012
+    try:
+        plot_income_by_region.graphical_explorer(year)
+    except loader_Error as e:
+        print str(e)
